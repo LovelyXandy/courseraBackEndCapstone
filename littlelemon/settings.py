1f12cae8db7431491c6f331864c0042f83d4a415
Dr A J Williamson <drajwilliamson@gmail.com>
2023-11-30 12:14:38 +0000

    updated settings

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reservation',
+    'rest_framework.authtoken',
+    'djoser',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
