591a6f78f8759a6e0b8a5c7bd506c1490a70cd6e
Dr A J Williamson <drajwilliamson@gmail.com>
2023-11-30 14:02:13 +0000

    reservation removed and added to restaurant

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservation',
-    'rest_framework.authtoken',
-    'djoser',
-    'rest_framework',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +55,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
